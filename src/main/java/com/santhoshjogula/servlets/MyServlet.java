ad999d44a0a63bc9b43b4167180b73de3571d2ef
Santhosh <santhoshkumar.jogula@gmail.com>
2024-01-09 02:09:20 +0530

    Invalid city input condition added.

--- src/main/java/com/santhoshjogula/servlets/MyServlet.java
+++ src/main/java/com/santhoshjogula/servlets/MyServlet.java
@@ -1,27 +1,30 @@
-package main.servlets;
+package com.santhoshjogula.servlets;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Date;
+import java.util.Properties;
 import java.util.Scanner;
 
 /**
- * MyServlet implementation class MyServlet
+ * com.santhoshjogula.servlets.MyServlet implementation class com.santhoshjogula.servlets.MyServlet
  */
-@WebServlet("/MyServlet")
+@WebServlet("/weather")
 public class MyServlet extends HttpServlet {
+    @Serial
     private static final long serialVersionUID = 1L;
+    private static String apikey;
+    private static String apiBaseUrl;
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -31,11 +34,43 @@ public class MyServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+        // Get the path to the configuration file from the context parameters
+        String configFile = getServletContext().getInitParameter("configFile");
+
+        // Ensure the configFile parameter is not null
+        if (configFile == null) {
+            throw new ServletException("Config file path is not specified in context parameters.");
+        }
+
+        // Load properties from the configuration file
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream(configFile)) {
+            // Ensure that the input stream is not null before loading properties
+            if (input != null) {
+                Properties properties = new Properties();
+                properties.load(input);
+
+                // Retrieve values from the properties object
+                apikey = properties.getProperty("apiKey");
+                apiBaseUrl = properties.getProperty("apiBaseUrl");
+
+                // Use the retrieved values as needed
+                System.out.println("API Key: " + apikey);
+                System.out.println("API url: " + apiBaseUrl);
+            } else {
+                throw new ServletException("Config file not found: " + configFile);
+            }
+        } catch (IOException e) {
+            throw new ServletException("Error loading configuration file", e);
+        }
+    }
+
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
         response.sendRedirect("index.html");
     }
 
@@ -43,19 +78,29 @@ public class MyServlet extends HttpServlet {
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        //API Key
-        String apiKey = "215f92c27389948d3a31c8255cb1bd85";
         // Get the city from the form input
         String city = request.getParameter("city");
 
         // Create the URL for the OpenWeatherMap API request
-        String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+        String apiUrl = apiBaseUrl + city + "&appid=" + apikey;
 
         try {
             URL url = new URL(apiUrl);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
+
+            // Check the HTTP response code
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == 404) {
+                // Set an attribute indicating the error
+                //request.setAttribute("error", "Invalid city name. Please enter a valid city name.");
+                request.setAttribute("errorMessage", "Invalid city name entered. Please enter another city name.");
+
+                // Forward the request to the weather.jsp page for rendering
+                request.getRequestDispatcher("index.jsp").forward(request, response);
+                return; // Exit the method to avoid further processing
+            }
 
             InputStream inputStream = connection.getInputStream();
             InputStreamReader reader = new InputStreamReader(inputStream);
@@ -109,6 +154,13 @@ public class MyServlet extends HttpServlet {
 
             // Close the connection
             connection.disconnect();
+
+        } catch (FileNotFoundException e) {
+            // Set an attribute indicating the error
+            //request.setAttribute("error", "City not found. Please enter a valid city name.");
+            request.setAttribute("errorMessage", "Invalid city name entered. Please enter another city name.");
+            // Forward the request to the weather.jsp page for rendering
+            request.getRequestDispatcher("index.jsp").forward(request, response);
         } catch (IOException e) {
             e.printStackTrace();
         }
